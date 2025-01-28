df59a8f65fcbff3dd49d57a0169eac29a11192ce
goobercoder <vaino.o.hanninen@gmail.com>
2025-01-28 18:14:54 +0200

    js stuff

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 document.getElementById("start-game-btn").addEventListener('click', start_gaem);
 
 function start_gaem(){
-    console.log("kill me");
-    document.writeln("work in procrastination");
+    document.getElementById("intro-screen").style.display = 'none';
+    document.getElementById("game-screen").style.display = 'block';
 }
